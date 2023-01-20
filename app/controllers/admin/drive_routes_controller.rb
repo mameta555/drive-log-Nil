04e0ab8f9b43fa7b435e90a13b619a56a3efd728
EC2 Default User <ec2-user@ip-172-31-15-158.ap-northeast-1.compute.internal>
2023-01-20 01:00:24 +0000

    管理者側投稿詳細作成

--- app/controllers/admin/drive_routes_controller.rb
+++ app/controllers/admin/drive_routes_controller.rb
@@ -3,4 +3,13 @@ class Admin::DriveRoutesController < ApplicationController
     @drive_routes = DriveRoute.page(params[:page]).per(9).order(created_at: :desc)
   end
 
+  def show
+    @drive_route = DriveRoute.find(params[:id])
+  end
+
+  def destroy
+    @drive_route = DriveRoute.find(params[:id])
+    @drive_route.destroy
+    redirect_to admin_drive_routes_path
+  end
 end
